0b570de235e18babf90506b87ea29270810ae3c0
Emre Sezer <1997.emresezer@gmail.com>
2023-03-10 14:47:40 +0300

    Initial commit

---
+++ src/main/java/com/emresezer/reactspringfullstack/ReactSpringFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.emresezer.reactspringfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emresezer/reactspringfullstack/ReactSpringFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emresezer.reactspringfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
